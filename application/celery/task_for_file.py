62b31ae4c7ecb557bde8080b9d442bed7571d870
Dmitry Pechenkin <lupuss26@gmail.com>
2023-08-14 19:26:00 +0300

    delete comments

--- application/celery/task_for_file.py
+++ application/celery/task_for_file.py
@@ -1,7 +1,7 @@
 from celery import Celery
+
 from application.db_app.settings import settings
 
-print(f"amqp://{settings.RABBITMQ_DEFAULT_USER}:{settings.RABBITMQ_DEFAULT_PASS}@{settings.RABBITMQ_HOST}:{settings.REDIS_PORT}//")
 
 task = Celery('task_for_file',
               broker=f"amqp://{settings.RABBITMQ_DEFAULT_USER}:{settings.RABBITMQ_DEFAULT_PASS}@{settings.RABBITMQ_HOST}:{settings.RABBITMQ_PORT}//")
@@ -9,7 +9,6 @@ task = Celery('task_for_file',
 task.conf.beat_schedule = {
     'add-every-15-seconds': {
         'task': 'application.admin.tasks.sync_run',
-        #'task': '..admin.tasks.sync_run',
         'schedule': 15.0,
     },
 }
