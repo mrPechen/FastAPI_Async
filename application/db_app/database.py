35b3c489b8741609722839b4b5bcf6e87c194346
Dmitry Pechenkin <lupuss26@gmail.com>
2023-08-14 19:26:52 +0300

    sort imports

--- application/db_app/database.py
+++ application/db_app/database.py
@@ -2,9 +2,9 @@ from typing import AsyncGenerator
 
 from sqlalchemy import NullPool
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 from application.db_app.settings import settings
-from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = f'postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:5432/{settings.POSTGRES_DB}'
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, poolclass=NullPool)
