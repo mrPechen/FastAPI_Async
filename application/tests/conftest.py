fd02068cd1d04d6270ede770219ec5ba635154a0
Dmitry Pechenkin <lupuss26@gmail.com>
2023-08-18 08:47:59 +0300

    Update conftest.py

--- application/tests/conftest.py
+++ application/tests/conftest.py
@@ -1,10 +1,11 @@
 import asyncio
+from typing import Any
 
 import pytest
 
 
 @pytest.fixture(autouse=True)
-def event_loop():
+def event_loop() -> Any:
     try:
         loop = asyncio.get_running_loop()
     except RuntimeError:
